1e54e59585b214922d8547bef64c91c2e9c35ba9
Mdominykas <marmadominykas@gmail.com>
2023-05-03 16:19:27 +0300

    removed-uneeded-pi-value

--- kodas1/src/Constants/PIValues.java
+++ kodas1/src/Constants/PIValues.java
@@ -5,5 +5,4 @@ public class PIValues {
     public static int DivisionByZero = 1;
     public static int IncorrectAddress = 2;
     public static int InvalidOperation = 3;
-    public static int Overflow = 4;
 }
