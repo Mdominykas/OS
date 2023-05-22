258f6bb9fc7b62346a75db6a752d7869eddd4b68
Mdominykas <marmadominykas@gmail.com>
2023-05-22 18:30:30 +0300

    improved-priorities

--- kodas2/src/Processes/ProcessesPriority.java
+++ kodas2/src/Processes/ProcessesPriority.java
@@ -2,24 +2,13 @@ package Processes;
 
 public class ProcessesPriority {
     public static int StartStop = 20;
-    public static int ReadFromInterface = 20;
-    public static int JCL = 20;
-    public static int MainProc = 20;
-    public static int JobGovernor = 20;
-    public static int FileSystem = 20;
-    public static int PrintLine = 20;
-    public static int Interrupt = 20;
-    public static int VirtualMachine = 20;
-    public static int Idle = 20;
-//    public static int StartStop = 20;
-//    public static int ReadFromInterface = 19;
-//    public static int JCL = 18;
-//    public static int MainProc = 17;
-//    public static int Loader = 16;
-//    public static int JobGovernor = 15;
-//    public static int FileSystem = 14;
-//    public static int PrintLine = 13;
-//    public static int Interrupt = 12;
-//    public static int VirtualMachine = 11;
-//    public static int Idle = 1;
+    public static int ReadFromInterface = 19;
+    public static int JCL = 18;
+    public static int MainProc = 17;
+    public static int FileSystem = 16;
+    public static int PrintLine = 15;
+    public static int Interrupt = 14;
+    public static int JobGovernor = 13;
+    public static int VirtualMachine = 12;
+    public static int Idle = 1;
 }
